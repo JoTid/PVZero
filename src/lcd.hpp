38ce30ccada321f87b26a31ec9a4b9ef0e47a3b8
Johann Tiderko <johann@tiderko.de>
2024-02-12 19:51:58 +0100

    Update LCD implementation, convert it to a class

--- src/lcd.hpp
+++ src/lcd.hpp
@@ -1,12 +1,38 @@
-/**
- * @brief Initialisation of LCD and all required resources
- * 
- */
-void lcdInit(void);
+/*--------------------------------------------------------------------------------------------------------------------*\
+** Definitions and Enums                                                                                              **
+**                                                                                                                    **
+\*--------------------------------------------------------------------------------------------------------------------*/ 
 
-/**
- * @brief 
- * 
- * @param slValueS 
- */
-void lcdProcess(int slValueS);
+
+/*--------------------------------------------------------------------------------------------------------------------*\
+** Declaration                                                                                                        **
+**                                                                                                                    **
+\*--------------------------------------------------------------------------------------------------------------------*/ 
+
+class LCD
+{
+private:
+   /* data */
+   typedef enum lcd_e {
+      eLCD_AppInit = 0,
+      eLCD_AppRunOK,
+      eLCD_AppRunFailure,
+   } lcd_te;
+
+   void busy(void);
+   void warning(void);
+   
+   void progressBar3(void);
+
+   /**
+    * @brief Refresh time of LCD given in milliseconds
+    */
+   #define  LCD_REFRESH_TIME  20
+
+public:
+
+   LCD(void);
+   ~LCD(void);
+   void init(void);
+   void process(void);
+};
